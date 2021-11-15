9c435103f96c870d087814a1ddc03bf75dfa3171
Mikhail <m.utkin@zaorotec.ru>
2021-11-15 13:48:58 +0300

    Подключил контейнеры.

--- app/Http/Controllers/v1/Action/Test/MyTestAction.php
+++ app/Http/Controllers/v1/Action/Test/MyTestAction.php
@@ -5,19 +5,18 @@ namespace App\Http\Controllers\v1\Action\Test;
 use App\Exceptions\http\NotFoundHttpException;
 use App\Exceptions\NotFoundException;
 use App\Http\Controllers\Controller;
-use App\Repositories\Test\TestRepository;
 use App\Services\tests\MyTestService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\App;
 
 class MyTestAction extends Controller
 {
-    /* @var $service*/
+    /* @var $service MyTestService */
     public $service;
 
     public function __construct()
     {
-        $this->service = new MyTestService(new TestRepository(DB::connection()->getPdo()));
+        $this->service = App::make(MyTestService::class);
     }
 
     /**
